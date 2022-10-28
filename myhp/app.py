31d209870854e4da35dd244ab581397839a4df6d
Shion <shion24stat@gmail.com>
2022-10-28 16:39:29 +0900

    fixed app.py

--- myhp/app.py
+++ myhp/app.py
@@ -8,8 +8,26 @@ app = Flask(__name__)
 @app.route("/")
 def index() :
     return render_template(
-        "index.html"
+        "index/index.html"
+    )
+
+@app.route("/contents/dev/index")
+def dev_index() :
+    return render_template(
+        "dev/index.html"
+    )
+
+@app.route("/contents/rls/index")
+def rls_index() :
+    return render_template(
+        "rls/index.html"
+    )
+
+@app.route("/contents/apis/index")
+def apis_index() :
+    return render_template(
+        "apis/index.html"
     )
 
 if __name__ == "__main__" :
-    app.run()
+    app.run(debug=True)
